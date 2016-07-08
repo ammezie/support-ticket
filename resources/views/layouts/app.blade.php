5f77bf0646efb885b193905ef31248bc6a0b61aa
Chimezie Enyinnaya <meziemichael@gmail.com>
2016-07-08 13:02:24 +0100

    remove home link from navigation menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,17 +45,6 @@
             </div>
 
             <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                <!-- Left Side Of Navbar -->
-                <ul class="nav navbar-nav">
-                    <li><a href="{{ url('/') }}">Home</a></li>
-                    @if (Auth::user()->is_admin)
-                        <li><a href="{{ url('admin/tickets') }}">Tickets</a></li>
-                    @else
-                        <li><a href="{{ url('my_tickets') }}">My Tickets</a></li>
-                        <li><a href="{{ url('new_ticket') }}">Open Ticket</a></li>
-                    @endif
-                </ul>
-
                 <!-- Right Side Of Navbar -->
                 <ul class="nav navbar-nav navbar-right">
                     <!-- Authentication Links -->
@@ -63,6 +52,12 @@
                         <li><a href="{{ url('/login') }}">Login</a></li>
                         <li><a href="{{ url('/register') }}">Register</a></li>
                     @else
+                        @if (Auth::user()->is_admin)
+                            <li><a href="{{ url('admin/tickets') }}">Tickets</a></li>
+                        @else
+                            <li><a href="{{ url('my_tickets') }}">My Tickets</a></li>
+                            <li><a href="{{ url('new_ticket') }}">Open Ticket</a></li>
+                        @endif
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                 {{ Auth::user()->name }} <span class="caret"></span>
