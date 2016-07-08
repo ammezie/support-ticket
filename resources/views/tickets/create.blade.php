d630aff44c4709dd4f63df75865fb153e7fe6c6f
Chimezie Enyinnaya <meziemichael@gmail.com>
2016-07-08 16:01:53 +0100

    correct create ticket form action

--- resources/views/tickets/create.blade.php
+++ resources/views/tickets/create.blade.php
@@ -11,7 +11,7 @@
 	            <div class="panel-body">
                     @include('includes.flash')
 
-	                <form class="form-horizontal" role="form" method="POST" action="{{ url('/tickets') }}">
+	                <form class="form-horizontal" role="form" method="POST" action="{{ url('/new_ticket') }}">
                         {!! csrf_field() !!}
 
                         <div class="form-group{{ $errors->has('title') ? ' has-error' : '' }}">
